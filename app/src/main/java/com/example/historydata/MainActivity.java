679bb6894539cdfc671a94ff302f47a40eb564c3
mainstring <fnnpkr@gmail.com>
2020-05-16 01:41:42 +0600

    step data fetched

--- app/src/main/java/com/example/historydata/MainActivity.java
+++ app/src/main/java/com/example/historydata/MainActivity.java
@@ -1,23 +1,66 @@
 package com.example.historydata;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.historydata.permissions.Permissions;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.Value;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.request.DataSourcesRequest;
+import com.google.android.gms.fitness.request.OnDataPointListener;
+import com.google.android.gms.fitness.request.SensorRequest;
+import com.google.android.gms.fitness.result.DataReadResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static java.text.DateFormat.getDateInstance;
+import static java.text.DateFormat.getTimeInstance;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "BasicSensorsApi";
 
     //Request Code for Activity
     private static final int REQUEST_CODE = 1;
+    private static final int REQUEST_OAUTH_REQUEST_CODE = 1;
+
+    // [START mListener_variable_reference]
+    // Need to hold a reference to this listener, as it's passed into the "unregister"
+    // method in order to stop all sensors from sending data to this listener.
+    private OnDataPointListener mListener;
+    // [END mListener_variable_reference]
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +94,163 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            Permissions.with(this)
+                    .request(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACTIVITY_RECOGNITION)
+                    .withPermanentDenialDialog("Fitness app requires these permissions")
+                    .ifNecessary()
+                    .onAllGranted(this::findFitnessDataSourcesWrapper)
+                    .onAnyDenied(this::finish)
+                    .execute();
+        } else {
+
+            Permissions.with(this)
+                    .request(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    .withPermanentDenialDialog("Fitness app requires these permissions")
+                    .ifNecessary()
+                    .onAllGranted(this::findFitnessDataSourcesWrapper)
+                    .onAnyDenied(this::finish)
+                    .execute();
+        }
+//        findFitnessDataSourcesWrapper();
+    }
+
+
+    /** Launches the Google SignIn activity to request OAuth permission for the user. */
+    private void requestOAuthPermission() {
+        FitnessOptions fitnessOptions = getFitnessSignInOptions();
+        GoogleSignIn.requestPermissions(
+                this,
+                REQUEST_OAUTH_REQUEST_CODE,
+                GoogleSignIn.getLastSignedInAccount(this),
+                fitnessOptions);
+    }
+
+    /**
+     * A wrapper for {@link #findFitnessDataSources}. If the user account has OAuth permission,
+     * continue to {@link #findFitnessDataSources}, else request OAuth permission for the account.
+     */
+    private void findFitnessDataSourcesWrapper() {
+        if (hasOAuthPermission()) {
+//            findFitnessDataSources();
+            readStepCount();
+        } else {
+            requestOAuthPermission();
+        }
+    }
+
+    /** Gets the {@link FitnessOptions} in order to check or request OAuth permission for the user. */
+    private FitnessOptions getFitnessSignInOptions() {
+        return FitnessOptions.builder()
+                .addDataType(DataType.TYPE_LOCATION_SAMPLE)
+                .addDataType(DataType.TYPE_ACTIVITY_SEGMENT)
+                .build();
+    }
+
+    /** Checks if user's account has OAuth permission to Fitness API. */
+    private boolean hasOAuthPermission() {
+        FitnessOptions fitnessOptions = getFitnessSignInOptions();
+        return GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions);
+    }
+    /** Finds available data sources and attempts to register on a specific {@link DataType}. */
+    private void findFitnessDataSources() {
+        // [START find_data_sources]
+        // Note: Fitness.SensorsApi.findDataSources() requires the ACCESS_FINE_LOCATION permission.
+        Fitness.getSensorsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .findDataSources(
+                        new DataSourcesRequest.Builder()
+                                .setDataTypes(DataType.TYPE_LOCATION_SAMPLE)
+                                .setDataSourceTypes(DataSource.TYPE_RAW)
+                                .build())
+                .addOnSuccessListener(
+                        new OnSuccessListener<List<DataSource>>() {
+                            @Override
+                            public void onSuccess(List<DataSource> dataSources) {
+                                for (DataSource dataSource : dataSources) {
+                                    Log.i(TAG, "Data source found: " + dataSource.toString());
+                                    Log.i(TAG, "Data Source type: " + dataSource.getDataType().getName());
+
+                                    // Let's register a listener to receive Activity data!
+                                    if (dataSource.getDataType().equals(DataType.TYPE_LOCATION_SAMPLE)
+                                            && mListener == null) {
+                                        Log.i(TAG, "Data source for LOCATION_SAMPLE found!  Registering.");
+//                                        registerFitnessDataListener(dataSource, DataType.TYPE_LOCATION_SAMPLE);
+                                    }
+                                }
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e(TAG, "failed", e);
+                            }
+                        });
+        // [END find_data_sources]
+    }
+
+    private void readStepCount () {
+        // Setting a start and end date using a range of 1 week before this moment.
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+
+        java.text.DateFormat dateFormat = getDateInstance();
+        Log.i(TAG, "Range Start: " + dateFormat.format(startTime));
+        Log.i(TAG, "Range End: " + dateFormat.format(endTime));
+
+        DataReadRequest readRequest =
+                new DataReadRequest.Builder()
+                        // The data request can specify multiple data types to return, effectively
+                        // combining multiple data queries into one call.
+                        // In this example, it's very unlikely that the request is for several hundred
+                        // datapoints each consisting of a few steps and a timestamp.  The more likely
+                        // scenario is wanting to see how many steps were walked per day, for 7 days.
+                        .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                        // Analogous to a "Group By" in SQL, defines how data should be aggregated.
+                        // bucketByTime allows for a time span, whereas bucketBySession would allow
+                        // bucketing by "sessions", which would need to be defined in code.
+                        .bucketByTime(1, TimeUnit.DAYS)
+                        .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                        .build();
+
+        Task<DataReadResponse> response = Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this)).readData(readRequest);
+        response.addOnCompleteListener(this, new OnCompleteListener<DataReadResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<DataReadResponse> task) {
+
+                if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "successful", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "failed", Toast.LENGTH_LONG).show();
+                }
+                if(!task.isSuccessful()) return;
+                List<DataSet> dataSets = task.getResult().getDataSets();
+                Toast.makeText(MainActivity.this, "" + dataSets.size(), Toast.LENGTH_LONG).show();
+                for (DataSet dataSet: dataSets) {
+                    dumpDataSet(dataSet);
+                }
+            }
+        });
+    }
+
+    private static void dumpDataSet(DataSet dataSet) {
+        Log.i(TAG, "Data returned for Data type: " + dataSet.getDataType().getName());
+        DateFormat dateFormat = getTimeInstance();
+
+        for (DataPoint dp : dataSet.getDataPoints()) {
+            Log.i(TAG, "Data point:");
+            Log.i(TAG, "\tType: " + dp.getDataType().getName());
+            Log.i(TAG, "\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+            Log.i(TAG, "\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)));
+            for (Field field : dp.getDataType().getFields()) {
+                Log.i(TAG, "\tField: " + field.getName() + " Value: " + dp.getValue(field));
+            }
+        }
     }
 
     private void requestPermission() {
@@ -84,6 +284,16 @@ public class MainActivity extends AppCompatActivity {
             }else{
                 Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
 
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_OAUTH_REQUEST_CODE) {
+//                findFitnessDataSources();
+                readStepCount();
             }
         }
     }
---
+++ app/src/main/java/com/example/historydata/concurrent/AssertedSuccessListener.java
@@ -0,0 +1,10 @@
+package com.example.historydata.concurrent;
+
+import java.util.concurrent.ExecutionException;
+
+public abstract class AssertedSuccessListener<T> implements ListenableFuture.Listener<T> {
+    @Override
+    public void onFailure(ExecutionException e) {
+        throw new AssertionError(e);
+    }
+}
